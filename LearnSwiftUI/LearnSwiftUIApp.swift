4b50ee47dc387e7b31664b7617d3d48fd6dc913d
Asif Khan <asif.khan@appinventiv.com>
2022-02-25 19:05:38 +0530

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Admin on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
